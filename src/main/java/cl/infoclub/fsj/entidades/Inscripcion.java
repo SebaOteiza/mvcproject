260facea2d6e026fe2d373b216e0c2dd1a578f72
gonzalezlillo <gonzalezlillo@yahoo.com>
2022-10-26 22:02:38 -0300

    entity Inscripcion.java

--- src/main/java/cl/infoclub/fsj/entidades/Inscripcion.java
+++ src/main/java/cl/infoclub/fsj/entidades/Inscripcion.java
@@ -1,5 +1,45 @@
 package cl.infoclub.fsj.entidades;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+@Table(name = "inscripcion")
 public class Inscripcion {
 
+	@Id
+	@Column(name = "id_inscripcion")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  // para que el id sea autoincremental
+	private int idInscripcion;
+	
+	// cuando el nombre del atributo de la entidad y el
+	// nombre de la columna de la tabla, se puede eliminar
+	// el paréntesis con el name después de @Column
+	@Basic
+	@Column
+	private String nombre;
+	
+	@Basic
+	@Column
+	private int telefono;
+	
+	@Basic
+	@Column(name = "id_curso")
+	private int idCurso;
+	
+	@Basic
+	@Column(name = "id_forma_pago")
+	private int idFormaPago;
+	
 }
